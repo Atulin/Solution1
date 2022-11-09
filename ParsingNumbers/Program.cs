536c71cf189082cdc72d676d8f38752bf84b0604
Atulin <11233299+Atulin@users.noreply.github.com>
2022-11-09 06:34:39 +0100

    Number converters

---
+++ ParsingNumbers/Program.cs
@@ -0,0 +1,61 @@
+﻿var arr = new[] {"0", null, "179", "kookaburra"};
+var functions = new (string name, ConsoleColor color, Action<string?> fun)[]
+{
+    (nameof(WithConvert), ConsoleColor.Green, WithConvert),
+    (nameof(WithParse), ConsoleColor.Red, WithParse),
+    (nameof(WithTryParse), ConsoleColor.Blue, WithTryParse)
+};
+
+foreach (var fun in functions)
+{
+    Console.ForegroundColor = fun.color;
+    Console.WriteLine(fun.name);
+    foreach (var num in arr)
+    {
+        Console.Write($"| {num ?? "null",-10} | ");
+        fun.fun(num);
+    }
+    Console.ResetColor();
+}
+
+
+void WithConvert(string? input)
+{
+    try
+    {
+        var num = Convert.ToInt32(input);
+        Console.WriteLine($"Your number times two is {num * 2}");
+    }
+    catch
+    {
+        Console.WriteLine("An exception has occurred");
+    }
+}
+
+
+void WithParse(string? input)
+{
+    try
+    {
+        var num = int.Parse(input);
+        Console.WriteLine($"Your number times two is {num * 2}");
+    }
+    catch
+    {
+        Console.WriteLine("An exception has occurred");
+    }
+}
+
+
+void WithTryParse(string? input)
+{
+    if (int.TryParse(input, out var num))
+    {
+        Console.WriteLine($"Your number times two is {num * 2}");
+    }
+    else
+    {
+        Console.WriteLine("Not a number");
+    }
+    
+}
