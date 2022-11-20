92d58a334be1b5d77fed417d06b9652e228a29cd
Atulin <11233299+Atulin@users.noreply.github.com>
2022-11-20 06:04:41 +0100

    Added dynamics benchmark

---
+++ Dynamics/Benchmark.cs
@@ -0,0 +1,86 @@
+﻿using System.Text.Json;
+using BenchmarkDotNet.Attributes;
+
+namespace Dynamics;
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    private const string Json = """
+            {
+                "Name": "Bob",
+                "Age": 43,
+                "ForkliftCertified": true,
+                "Address": {
+                    "Street": "Daisy Boulevard",
+                    "Number": 23
+                }
+            }
+            """;
+
+    [Benchmark]
+    public string BenchDynamic()
+    {
+        var person = JsonSerializer.Deserialize<dynamic>(Json);
+        
+        if (person is null) return "null";
+
+        var p = (JsonElement)person;
+
+        var gotName = p.TryGetProperty("Name", out var name);
+        var gotForkliftCertification = p.TryGetProperty("ForkliftCertified", out var forkliftCertified);
+
+        var gotAddress = p.TryGetProperty("Address", out var address);
+
+        var gotStreet = address.TryGetProperty("Street", out var street);
+        var gotNumber = address.TryGetProperty("Number", out var number);
+
+        if (!gotName || !gotForkliftCertification || !gotAddress || !gotStreet || !gotNumber) return "That's why you don't do that";
+
+        return $"{name.GetString()} {(forkliftCertified.GetBoolean() ? "is" : "is not")} forklift certified and lives at {street.GetString()} {number.GetInt32()}";
+        
+    }
+
+    [Benchmark]
+    public string BenchDictionary()
+    {
+        var person = JsonSerializer.Deserialize<Dictionary<string, object>>(Json);
+        
+        if (person is null) return "null";
+
+        var address = (JsonElement)person["Address"];
+
+        var gotStreet = address.TryGetProperty("Street", out var street);
+        var gotNumber = address.TryGetProperty("Number", out var number);
+
+        if (!gotStreet || !gotNumber) return "Couldn't get street or number";
+
+        return $"{person["Name"]} {(((JsonElement)person["ForkliftCertified"]).GetBoolean() ? "is" : "is not")} forklift certified and lives at {street.GetString()} {number.GetInt32()}";
+        
+    }
+
+    [Benchmark]
+    public string BenchClass()
+    {
+        var person = JsonSerializer.Deserialize<Person>(Json);
+        
+        if (person is null) return "null";
+        
+        return $"{person.Name} {(person.ForkliftCertified ? "is" : "is not")} forklift certified and lives at {person.Address.Street} {person.Address.Number}";
+
+    }
+
+    public class Person
+    {
+        public required string Name { get; init; }
+        public required int Age { get; init; }
+        public required bool ForkliftCertified { get; init; }
+        public required Address Address { get; init; }
+    }
+
+    public class Address
+    {
+        public required string Street { get; init; }
+        public required int Number { get; init; }
+    }
+}
---
+++ Dynamics/README.md
@@ -0,0 +1,30 @@
+﻿# Dynamics
+
+```
+BenchmarkDotNet=v0.13.2, OS=Windows 10 (10.0.19045.2251)
+AMD Ryzen 5 1600, 1 CPU, 12 logical and 6 physical cores
+.NET SDK=7.0.100
+  [Host]     : .NET 7.0.0 (7.0.22.51805), X64 RyuJIT AVX2
+  DefaultJob : .NET 7.0.0 (7.0.22.51805), X64 RyuJIT AVX2
+
+
+|          Method |     Mean |     Error |    StdDev |   Gen0 | Allocated |
+|---------------- |---------:|----------:|----------:|-------:|----------:|
+|    BenchDynamic | 2.274 us | 0.0335 us | 0.0344 us | 0.1755 |     736 B |
+| BenchDictionary | 3.000 us | 0.0461 us | 0.0431 us | 0.3777 |    1584 B |
+|      BenchClass | 1.538 us | 0.0232 us | 0.0205 us | 0.2060 |     864 B |
+
+// * Hints *
+Outliers
+  Benchmark.BenchDynamic: Default -> 3 outliers were removed (2.42 us..2.49 us)
+  Benchmark.BenchClass: Default   -> 1 outlier  was  removed (1.61 us)
+
+// * Legends *
+  Mean      : Arithmetic mean of all measurements
+  Error     : Half of 99.9% confidence interval
+  StdDev    : Standard deviation of all measurements
+  Gen0      : GC Generation 0 collects per 1000 operations
+  Allocated : Allocated memory per single operation (managed only, inclusive, 1KB = 1024B)
+  1 us      : 1 Microsecond (0.000001 sec)
+
+```
